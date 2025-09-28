e816cd05d7e32d952a65db80e87bb242f57d2488
Andrey-gift10 <csgo82448@gmail.com>
2025-09-28 19:42:30 +0300

    улучшили генератор паролей

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 alfavit =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 n = int(input("Введите длину пароля "))
+while n < 8:
+    print("Пароль должен быть больше 8 символов")
+    n = int(input("Введите длину пароля "))
 password = ""
 for i in range(n):
     password += random.choice(alfavit)
